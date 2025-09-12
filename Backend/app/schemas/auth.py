f1125aa11233186201d617d9d05854772bbd20e3
Ruben de la Cruz Jr <ruben.delacruzjr01@gmail.com>
2025-09-12 13:32:31 -0500

    Restarting session
    Need to start from scratch. Issues with frameworks

--- Backend/app/schemas/auth.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel, EmailStr
-class RegisterIn(BaseModel): email: EmailStr; password: str; name: str | None = None
-class LoginIn(BaseModel): email: EmailStr; password: str
-class TokenOut(BaseModel): accessToken: str
